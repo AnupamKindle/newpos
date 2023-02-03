5612531fc75cf9f1ae92275bef24d6f58ae30034
Maninder Kaur <maninder.kaur12@outlook.com>
2023-02-03 10:30:08 +0530

    Old query for test

--- src/main/java/com/kindlebit/pos/repository/CustomerRepository.java
+++ src/main/java/com/kindlebit/pos/repository/CustomerRepository.java
@@ -11,6 +11,10 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
     Optional<Customer> findByPhoneNumber(String phoneNumber);
 
+
+/*   @Query(value = "SELECT * FROM customer c WHERE c.name like '%:name%' and c.phone_number=':phoneNumber' ", nativeQuery = true)
+    Optional<Customer> findByNameAndPhoneNumber(@Param("name") String name, @Param("phoneNumber")String phoneNumber);*/
+
     Optional<Customer> findByName(String name);
 
 }
